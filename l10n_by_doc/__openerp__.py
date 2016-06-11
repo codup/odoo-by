188f37825ae2ea291849e493e103ea906f4229b2
wowas18 <wowas18@nm.ru>
2016-06-11 10:12:45 +0400

    Initial version

---
+++ l10n_by_doc/__openerp__.py
@@ -0,0 +1,29 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo
+#    Copyright (C) 2016 CodUP (<http://codup.com>).
+#
+##############################################################################
+
+{
+    'name': 'Belarus - Documents',
+    'version': '0.0',
+    'summary': 'Первичные документы',
+    'description': """
+!!!Don't download. The module under development!!!
+The module for print documents in accordance laws of Belarus.
+============================================================
+Возможности:
+    * Пока нет
+    """,
+    'author': 'CodUP',
+    'website': 'http://codup.com',
+    'category': 'Localization',
+    'sequence': 0,
+    'depends': [],
+    'demo': [],
+    'data': [],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
