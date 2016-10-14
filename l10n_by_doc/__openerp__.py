e514050606ea93e50b7fbac0375f82403739ea32
codup <wowas18@nm.ru>
2016-10-14 18:43:23 +0400

    change version

--- l10n_by_doc/__openerp__.py
+++ l10n_by_doc/__openerp__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Belarus - Documents',
-    'version': '0.5',
+    'version': '1.5',
     'summary': 'Первичные документы',
     'description': """
 The module for print documents in accordance laws of Belarus.
